5eebd66c77f3af392cd45e7113ad957a318beb83
Sebastian Müller <sebastian.mueller1208@gmail.com>
2025-05-07 09:39:49 +0000

    this is updated

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,11 +1,14 @@
 SELECT 
     i.orders_id
     ,date_date
-    ,SUM(margin + shipping_fee - logcost -ship_cost) AS operational_margin
-    ,SUM(revenue) AS revenue
-    ,SUM(quantity) AS quantity
-    ,SUM(purchase_cost) AS purchase_cost
-    ,SUM(margin)  AS margin
+    ,ROUND(margin + shipping_fee - logcost -ship_cost) AS operational_margin
+    ,i.revenue
+    ,i.quantity
+    ,i.purchase_cost
+    ,i.margin
+    ,i.shipping_fee
+    ,i.logcost
+    ,i.ship_cost
 FROM {{ ref('int_orders_margin')}} i
 LEFT JOIN {{ ref('stg_raw__ship')}} s
 ON i.orders_id=s.orders_id
